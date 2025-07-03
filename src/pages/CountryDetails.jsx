87852d872005c7cb22668fe3268e88a2b2afbf27
NaveenManickDev <naveen.devhub@gmail.com>
2025-07-03 08:55:58 +0530

    Country Details Component

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Loading from '../components/Loading'
 
 const CountryDetails = () => {
+  const {name} = useParams()
+  const [countryData, setContryData] = useState()
+  
+    const [isLoading, setLoading] = useState(true)
+  useEffect(()=>{
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setContryData(data[0])      
+    }).finally(()=>{
+      setLoading(false)
+    })
+  },[name])
+
+   if(isLoading) {
+    return <Loading />
+  }
+
   return (
-    <div>CountryDetails</div>
+    <div>
+      <h2>{countryData?.name?.common}</h2>
+      <img src={countryData?.flags?.svg} alt="" />
+      <p> <b>Capital</b>: {countryData?.capital?.[0]}</p>
+      <p> <b>Region</b>: {countryData?.region}</p>
+      <p><b>Population</b>: {countryData?.population}</p>
+      <p></p>
+    </div>
   )
 }
 
--- src/pages/CountryList.jsx
+++ src/pages/CountryList.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import Loading from '../components/Loading'
+import { Link } from 'react-router-dom'
 
 const CountryList = () => {
 
   const [countryDatas, setContryDatas] = useState([])
+
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(()=>{
     fetch("https://restcountries.com/v3.1/all?fields=name,flags")
@@ -10,18 +14,25 @@ const CountryList = () => {
     .then((data) => {
       console.log(data);
       setContryDatas(data)      
+    }).finally(()=>{
+      setLoading(false)
     })
   },[])
+
+  if(isLoading) {
+    return <Loading />
+  }
+
   return (
     <div>
       <h1 className='text-3xl font-bold mb-4'>Countries</h1>
       <div className="card-container grid grid-cols-4 gap-4">
         {countryDatas.map((country) => {
           return(
-              <div className="p-4 border rounded-xl shadow-md">
+              <Link to={`/country/${country.name.common}`} className="p-4 border rounded-xl shadow-md">
                 <img className='w-full h-32 object-contain' src={country.flags.svg} alt="Ironman" />
                 <h2>{country.name.common}</h2>
-              </div>
+              </Link>
           )
         })}
         {/* <div className="p-4 border rounded-xl shadow-md">
