8e9ce1625e2f7b036e6d32dfd7bfaa7e95e9c57e
NaveenManickDev <naveen.devhub@gmail.com>
2025-07-03 08:21:32 +0530

    first commit

---
+++ src/pages/CountryList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const CountryList = () => {
+
+  const [countryDatas, setContryDatas] = useState([])
+
+  useEffect(()=>{
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setContryDatas(data)      
+    })
+  },[])
+  return (
+    <div>
+      <h1 className='text-3xl font-bold mb-4'>Countries</h1>
+      <div className="card-container grid grid-cols-4 gap-4">
+        {countryDatas.map((country) => {
+          return(
+              <div className="p-4 border rounded-xl shadow-md">
+                <img className='w-full h-32 object-contain' src={country.flags.svg} alt="Ironman" />
+                <h2>{country.name.common}</h2>
+              </div>
+          )
+        })}
+        {/* <div className="p-4 border rounded-xl shadow-md">
+          <img className='w-full h-32 object-contain' src="https://cdna.artstation.com/p/assets/images/images/037/453/246/large/preet-iron-man-3.jpg?1620404652" alt="Ironman" />
+          <h2>Tony Stark</h2>
+        </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default CountryList
